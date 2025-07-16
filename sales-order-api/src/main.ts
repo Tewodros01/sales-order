e967377a9e5273a7938e1dd7276b2be33085d237
Tewodros01 <tewodrosm01@gmail.com>
2025-07-16 15:50:55 +0300

    Implment the task

--- sales-order-api/src/main.ts
+++ sales-order-api/src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
     cors: {
       origin: [
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174"
       ],
       credentials: true,
     },
